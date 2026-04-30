281a5f590f1e596646318452a006eabdf2e080c2
redskypub-eng <redskypub@gmail.com>
2026-04-30 15:43:06 +0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 export default async function handler(req, res) {
   try {
-    const target = "http://72.62.62.132:80"; // سرور خودت
+    const target = "http://72.62.62.132:80";
 
-    const response = await fetch(target, {
+    const url = target + req.url;
+
+    const response = await fetch(url, {
       method: req.method,
       headers: req.headers,
       body: req.method !== "GET" ? req.body : undefined
