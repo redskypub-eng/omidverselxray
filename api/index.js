5719d4b206ad2f4fba5733ae80eda0b8918c475e
redskypub-eng <redskypub@gmail.com>
2026-04-30 14:56:32 +0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,16 @@
+export default async function handler(req, res) {
+  try {
+    const url = req.query.url;
 
+    if (!url) {
+      return res.status(400).json({ error: "missing url" });
+    }
+
+    const response = await fetch(url);
+    const text = await response.text();
+
+    res.status(200).send(text);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}
